8117baeab20ddece324a89f83ab618cc83d96b1d
Imaad Ghoor <102949510+ImaadG@users.noreply.github.com>
2022-07-20 01:49:23 -0700

    keydown feature added
    added features that play the drum sounds when the required key is pressed

--- index.js
+++ index.js
@@ -2,43 +2,56 @@
 //     alert("I got clicked!");
 // });
 //safer to use drum instead of button incase we add more button to other elements
-for (var i=0; i<document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+//Detecting Button Press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         //alert("I got clicked!");
 
         var buttonInnerHTML = this.innerHTML; //stores the html element that is clicked
-
-            switch(buttonInnerHTML){
-                case "w":
-                    var audio = new Audio("sounds/crash.mp3");
-                    audio.play();
-                    break
-                case 'a':
-                    var audio = new Audio("sounds/kick-bass.mp3");
-                    audio.play();
-                    break;
-                case 's':
-                    var audio = new Audio("sounds/snare.mp3");
-                    audio.play();
-                    break;
-                case 'd':
-                    var audio = new Audio("sounds/tom-1.mp3");
-                    audio.play();
-                    break;
-                case 'j':
-                    var audio = new Audio("sounds/tom-2.mp3");
-                    audio.play();
-                    break;
-                case 'k':
-                    var audio = new Audio("sounds/tom-3.mp3");
-                    audio.play();
-                    break;
-                case 'l':
-                    var audio = new Audio("sounds/tom-4.mp3");
-                    audio.play();
-                    break;
-                default:
-            }
+        makeSound(buttonInnerHTML)
     });
 }
 
+//Detecting Keyboard Press
+
+document.addEventListener("keydown", function(event){ //play a sound assigned to letter of the drum if key is pressed
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break
+        case 'a':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+    }
+}
